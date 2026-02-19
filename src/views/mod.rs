fb5e0cb64bc4ea14e89b47e106f114bba4c6a36e
Roy Murphy <roycoins@gmail.com>
2026-02-19 12:01:36 +0000

    Update mod.rs

--- src/views/mod.rs
+++ src/views/mod.rs
@@ -1,4 +1,4 @@
 // src/views/mod.rs
-pub mod home;
-pub mod publish;
-pub mod detail;
+pub const HOME_HTML: &str = include_str!("home.html");
+pub const PUBLISH_HTML: &str = include_str!("publish.html");
+pub const DETAIL_HTML: &str = include_str!("detail.html");
