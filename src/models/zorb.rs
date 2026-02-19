83a68833085d3037057592b2fa875d24b6296ca2
Roy Murphy <roycoins@gmail.com>
2026-02-19 03:49:42 +0000

    Create zorb.rs

---
+++ src/models/zorb.rs
@@ -0,0 +1,27 @@
+use serde::{Deserialize, Serialize};
+use sqlx::FromRow;
+use uuid::Uuid;
+use chrono::{DateTime, Utc};
+
+#[derive(Debug, Clone, FromRow, Serialize, Deserialize)]
+pub struct Zorb {
+    pub id: Uuid,
+    pub name: String,
+    pub version: String,
+    pub description: Option<String>,
+    pub license: Option<String>,
+    pub repository: Option<String>,
+    pub owner_id: Option<Uuid>,
+    pub downloads: i64,
+    pub created_at: DateTime<Utc>,
+    pub updated_at: DateTime<Utc>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct NewZorb {
+    pub name: String,
+    pub version: String,
+    pub description: Option<String>,
+    pub license: Option<String>,
+    pub repository: Option<String>,
+}
