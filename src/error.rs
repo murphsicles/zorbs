df2687374d4a02629e1a9b24bb74ece10a605a78
Roy Murphy <roycoins@gmail.com>
2026-02-19 13:45:29 +0000

    Update error.rs

--- src/error.rs
+++ src/error.rs
@@ -1,21 +1,18 @@
 // src/error.rs
-use axum::response::{IntoResponse, Response};
 use axum::http::StatusCode;
-use serde_json::json;
-use axum::Json;
+use axum::response::{IntoResponse, Response};
 
 #[derive(Debug)]
 pub enum AppError {
-    Database(sqlx::Error),
-    NotFound,
+    BadRequest(String),
+    NotFound(String),
 }
 
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
-        let (status, message) = match self {
-            AppError::Database(_) => (StatusCode::INTERNAL_SERVER_ERROR, "Internal server error"),
-            AppError::NotFound => (StatusCode::NOT_FOUND, "Not found"),
-        };
-        (status, Json(json!({"error": message}))).into_response()
+        match self {
+            AppError::BadRequest(msg) => (StatusCode::BAD_REQUEST, msg).into_response(),
+            AppError::NotFound(msg) => (StatusCode::NOT_FOUND, msg).into_response(),
+        }
     }
 }
