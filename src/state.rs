3ccbc0362d16f7df42ff6bfecb74beff0030a031
Roy Murphy <roycoins@gmail.com>
2026-02-19 03:43:47 +0000

    Create state.rs

---
+++ src/state.rs
@@ -0,0 +1,23 @@
+use axum::extract::FromRef;
+use sqlx::PgPool;
+use std::sync::Arc;
+
+use crate::config;
+
+#[derive(Clone)]
+pub struct AppState {
+    pub db: PgPool,
+}
+
+pub fn new() -> Arc<AppState> {
+    let db = PgPool::connect_lazy(&config::database_url())
+        .expect("Failed to create DB pool");
+
+    Arc::new(AppState { db })
+}
+
+impl FromRef<Arc<AppState>> for PgPool {
+    fn from_ref(state: &Arc<AppState>) -> PgPool {
+        state.db.clone()
+    }
+}
