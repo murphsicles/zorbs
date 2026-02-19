0adedae075dd7b3ca4e6f5875324fb0e5777bd89
Roy Murphy <roycoins@gmail.com>
2026-02-19 04:00:21 +0000

    Create error.rs

---
+++ src/error.rs
@@ -0,0 +1,19 @@
+use axum::response::{IntoResponse, Response};
+use axum::http::StatusCode;
+use serde_json::json;
+
+#[derive(Debug)]
+pub enum AppError {
+    Database(sqlx::Error),
+    NotFound,
+}
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        let (status, message) = match self {
+            AppError::Database(_) => (StatusCode::INTERNAL_SERVER_ERROR, "Internal server error"),
+            AppError::NotFound => (StatusCode::NOT_FOUND, "Not found"),
+        };
+        (status, Json(json!({"error": message}))).into_response()
+    }
+}
