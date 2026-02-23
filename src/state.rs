90bc8717c2a9dbfb9b7986ce8d0121609f32df8a
Roy Murphy <roycoins@gmail.com>
2026-02-23 06:57:34 +0000

    Update state.rs

--- src/state.rs
+++ src/state.rs
@@ -1,6 +1,8 @@
 // src/state.rs
 use sqlx::PgPool;
 use std::sync::Arc;
+use tower_sessions_sqlx_store::PostgresStore;
+use tower_sessions::SessionManagerLayer;
 use crate::config;
 
 #[derive(Clone)]
@@ -8,8 +10,16 @@ pub struct AppState {
     pub db: PgPool,
 }
 
-pub fn new() -> Arc<AppState> {
+pub fn new() -> (Arc<AppState>, SessionManagerLayer<PostgresStore>) {
     let db = PgPool::connect_lazy(&config::database_url())
         .expect("Failed to create DB pool");
-    Arc::new(AppState { db })
+
+    let session_store = PostgresStore::new(db.clone());
+    let session_layer = SessionManagerLayer::new(session_store)
+        .with_secure(false) // true in prod
+        .with_http_only(true)
+        .with_same_site(tower_sessions::cookie::SameSite::Strict)
+        .with_expiry(tower_sessions::Expiry::OnInactivity(time::Duration::minutes(30)));
+
+    (Arc::new(AppState { db }), session_layer)
 }
